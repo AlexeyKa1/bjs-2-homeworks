a30d32278f706acd88def1f6697345eae62264cb
Алексей Калугин <kalugin_an@mail.ru>
2022-09-28 00:12:09 +0500

    tasks solved

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,49 @@
+// задача 1
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(','); 
+    let idx = cache.findIndex((item)=> item.hash === hash );
+    if (idx !== -1 ) { 
+        console.log("Из кэша: " + cache[idx].result); 
+        return "Из кэша: " + cache[idx].result;
+    }
+
+    let result = func(...args);
+    cache.push({hash, result});
+    if (cache.length > 5) { 
+      cache.shift()
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+// задача 2
+function debounceDecoratorNew(func, ms) {
+  let timerId = null;
+  function wrapper (...args){
+    if (timerId === null) {
+      func(...args);
+    }
+    clearTimeout(timerId);
+    timerId = setTimeout(() => timerId = null, ms);
+  }
+  return wrapper;
 }
 
+// задача 3
 function debounceDecorator2(func) {
-  // Ваш код
+  let timerId = null;
+  function wrapper(...args){
+    if (timerId === null) {
+      func(...args);
+    }
+    clearTimeout(timerId);
+    timerId = setTimeout(() => timerId = null, ms);
+    wrapper.count++;
+  }
+  return wrapper;
 }
